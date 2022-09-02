4f6ca180c556510fbb92f0e07098403d63841bb0
lgray52 <lgray52@charlottesvilleschools.org>
2022-09-02 14:04:48 -0400

    comment out code

--- raspberryPi/GrayLaunchpad1.py
+++ raspberryPi/GrayLaunchpad1.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-for i in range(10,0, -1):
-    print(i)
-    sleep(1)
-print("LIFTOFF")
+for i in range(10,0, -1):  # loop from 10-1, backwards by 1
+    print(i)  # print out count
+    sleep(1)  # one second between each count
+print("LIFTOFF") # once it counts down to 0, print liftoff
