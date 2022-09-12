fbcb7f3335d176122bf274afc0efc5108b8489cc
lgray52 <lgray52@charlottesvilleschools.org>
2022-09-12 14:11:20 -0400

    launchpad 4 finish

--- raspberryPi/GrayLaunchpad4Basic.py
+++ raspberryPi/GrayLaunchpad4Basic.py
@@ -15,15 +15,14 @@ ledRed.direction = digitalio.Direction.OUTPUT
 button = digitalio.DigitalInOut(board.GP0)
 button.pull = digitalio.Pull.UP  # set up button to be True when NOT pressed
 
-servoSetup = pwmio.PWMOut(board.GP28, duty_cycle = 2 ** 15, frequency = 50)  # servo set up
+servoSetup = pwmio.PWMOut(board.GP28, duty_cycle = 2 ** 15, frequency = 50)
 myServo = servo.Servo(servoSetup, min_pulse = 500, max_pulse = 2500)
 
-angle = 0  # set up variable for angle
-myServo.angle = angle
+myServo.angle = 0
 
 while True:
   if button.value == False:  # if button is pressed
-    for i in range(10, 3, -1):  # loop from 10-1, backwards by 1
+    for i in range(10, 0, -1):  # loop from 10-1, backwards by 1
         # if button.value == False:
         #     print("ABORT LAUNCH")
         #     while button.value == True:
@@ -34,22 +33,6 @@ while True:
         sleep(.5)  # one second between each count
         ledRed.value = False
         sleep(.5)  # blink off
-
-    for i in range(3, 0, -1):
-      print(i)
-      ledRed.value = True
-      angle_stop = angle + 30  # set stop point 30 degrees ahead of where angle is
-      for x in range(angle, angle_stop, 6):  # sweep servo for .5 seconds
-        myServo.angle = x
-        sleep(.1)
-      angle = angle_stop  # set current angle to where is stopped
-      ledRed.value = False
-      angle_stop = angle + 30  # set new stop for an additional 30 degrees
-      for x in range(angle, angle_stop, 6):  # sweep servo for the remaining .5 seconds
-        myServo.angle = x
-        sleep(.1)
-      angle = angle_stop  # and set angle to where it currently is again
-    
     print("LIFTOFF") # once it counts down to 0, print liftoff
     myServo.angle = 180
 
