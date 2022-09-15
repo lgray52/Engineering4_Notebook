5f3e0751e6273d4821128cdf83de4aab8ef7d5e1
lgray52 <lgray52@charlottesvilleschools.org>
2022-09-15 14:10:44 -0400

    add crash avoidance 1 and 2

--- raspberryPi/grayCrash2.py
+++ raspberryPi/grayCrash2.py
@@ -4,6 +4,7 @@ import adafruit_mpu6050 as imu
 import busio
 import board
 from time import sleep
+import digitalio
 
 sdaPin = board.GP2  # define which SDA & SCL pins to use - HAVE TO BE CONNECTED TO SAME I2C ON PICO
 sclPin = board.GP3
@@ -11,14 +12,19 @@ i2c = busio.I2C(sclPin, sdaPin)
 
 mpu = imu.MPU6050(i2c)  # set up mpu sensor accelerometer
 
+led = digitalio.DigitalInOut(board.GP16)  # connect led to board
+led.direction = digitalio.Direction.OUTPUT
+
 # in order to pull a fancy list (called a tuple) of the x, y, and z acceleration value in m/s^2, would call mpu.acceleration
 # for angular velocity, would call mpu.gyro
 
 while True:
     accelerationVals = mpu.acceleration
-    
     print(f"X acceleration: {accelerationVals[0]} m/s2")
     print(f"Y acceleration: {accelerationVals[1]} m/s2")
     print(f"Z acceleration: {accelerationVals[2]} m/s2")
     print("")
     sleep(.25)
+
+    if accelerationVals[2] <= 1:
+        led.value = True
