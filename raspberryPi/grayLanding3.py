cfc04a7133d1c6284bae5611da4fe9c3a0cc234a
lgray52 <lgray52@charlottesvilleschools.org>
2022-10-10 13:27:13 -0400

    fix indent

--- raspberryPi/grayLanding3.py
+++ raspberryPi/grayLanding3.py
@@ -26,55 +26,55 @@ display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 points = [['-50,-17,-57,12,-22,-7'],['28,-14,60,-7,54,18'],['45,30,51,-1,18,6'],['5,5,19,15,22,10']]
 
 triangleVals = []  # make a blank list to store area and centroids of triangles
-while True:
-    for i in range(len(points)):
-        x1, y1, x2, y2, x3, y3 = coordinateSeparator(points[i])  # separate the coordinates from each section of the list
-        vals = areaDistance(x1, y1, x2, y2, x3, y3)  # find the values from those vertices
-        triangleVals.append(vals)  # add the area and centroids of each triangle
-        area = vals[0]  # relevant area
 
-        splash = displayio.Group()  # make display group
+for i in range(len(points)):
+    x1, y1, x2, y2, x3, y3 = coordinateSeparator(points[i])  # separate the coordinates from each section of the list
+    vals = areaDistance(x1, y1, x2, y2, x3, y3)  # find the values from those vertices
+    triangleVals.append(vals)  # add the area and centroids of each triangle
+    area = vals[0]  # relevant area
 
-        areaText = label.Label(terminalio.FONT, text = f"{round(area, 2)} km2", color = 0xFFFF00, x = 5, y = 5)
-        splash.append(areaText)
-
-        xAxis = Line(64, 0, 64, 64, color = 0xFFFF00)  # draw the x-axis
-        splash.append(xAxis)
-
-        yAxis = Line(0, 32, 128, 32, color = 0xFFFF00)  # draw the y-axis
-        splash.append(yAxis)
-
-        origin = Circle(64, 32, 5, outline = 0xFFFF00)  # draw a little circle at the origin/base location
-        splash.append(origin)
-
-        # need to manipulate the coordinates so they are in the right place on the screen with respect to the center of the screen, (64, 32)
-        triangle = Triangle(int(x1) + 64, 32 - int(y1), int(x2) + 64, 32 - int(y2), int(x3) + 64, 32 - int(y3), outline = 0xFFFF00)
-        splash.append(triangle)  # plot the triangle with the given points. need integers because no decimal pixels
-
-        display.show(splash)
-
-        sleep(1)
-
-    for i in range(len(triangleVals)):  # delete triangles with area less than 100 square km
-        if triangleVals[i, 0] <= 100:
-            del triangleVals[i]
-            del points[i]
-
-    centroids = []  # blank list for centroid distances
-    for i in range(len(triangleVals)):
-        centroids.append(triangleVals[i, 1])  # add the centroid distances to list
-    
-    closest = min(centroids)  # find the minimum centroid distance
-    closestIndex = centroids.index(closest)  # find its index in the list, which corresponds to its index in the list of points
-
-    x1, y1, x2, y2, x3, y3 = coordinateSeparator(points[closestIndex])  # use the index found to get the points from the points list
-    areaPlusCentroid = areaDistance(x1, y1, x2, y2, x3, y3)  # get the distance and centroid
-
-    text = f"The closest suitable landing area has vertices ({x1}, {y1}), ({x2}, {y2}), and ({x3}, {y3}). The area is {areaPlusCentroid[0]} km2 and the centroid is {areaPlusCentroid[1]} km away from base."
-    
     splash = displayio.Group()  # make display group
 
-    showText = label.Label(terminalio.FONT, text = f"{round(area, 2)} km2", color = 0xFFFF00, x = 5, y = 5)
-    splash.append(showText)
+    areaText = label.Label(terminalio.FONT, text = f"{round(area, 2)} km2", color = 0xFFFF00, x = 5, y = 5)
+    splash.append(areaText)
+
+    xAxis = Line(64, 0, 64, 64, color = 0xFFFF00)  # draw the x-axis
+    splash.append(xAxis)
+
+    yAxis = Line(0, 32, 128, 32, color = 0xFFFF00)  # draw the y-axis
+    splash.append(yAxis)
+
+    origin = Circle(64, 32, 5, outline = 0xFFFF00)  # draw a little circle at the origin/base location
+    splash.append(origin)
+
+    # need to manipulate the coordinates so they are in the right place on the screen with respect to the center of the screen, (64, 32)
+    triangle = Triangle(int(x1) + 64, 32 - int(y1), int(x2) + 64, 32 - int(y2), int(x3) + 64, 32 - int(y3), outline = 0xFFFF00)
+    splash.append(triangle)  # plot the triangle with the given points. need integers because no decimal pixels
 
     display.show(splash)
+
+    sleep(1)
+
+for i in range(len(triangleVals)):  # delete triangles with area less than 100 square km
+    if triangleVals[i, 0] <= 100:
+        del triangleVals[i]
+        del points[i]
+
+centroids = []  # blank list for centroid distances
+for i in range(len(triangleVals)):
+    centroids.append(triangleVals[i, 1])  # add the centroid distances to list
+    
+closest = min(centroids)  # find the minimum centroid distance
+closestIndex = centroids.index(closest)  # find its index in the list, which corresponds to its index in the list of points
+
+x1, y1, x2, y2, x3, y3 = coordinateSeparator(points[closestIndex])  # use the index found to get the points from the points list
+areaPlusCentroid = areaDistance(x1, y1, x2, y2, x3, y3)  # get the distance and centroid
+
+text = f"The closest suitable landing area has vertices ({x1}, {y1}), ({x2}, {y2}), and ({x3}, {y3}). The area is {areaPlusCentroid[0]} km2 and the centroid is {areaPlusCentroid[1]} km away from base."
+    
+splash = displayio.Group()  # make display group
+
+showText = label.Label(terminalio.FONT, text = f"{round(area, 2)} km2", color = 0xFFFF00, x = 5, y = 5)
+splash.append(showText)
+
+display.show(splash)
