8d9fd49e4cbbc4706559fce193b7dbbfd466e4d6
lgray52 <lgray52@charlottesvilleschools.org>
2022-10-11 14:21:34 -0400

    morse code 1

---
+++ raspberryPi/grayMorseCode1.py
@@ -0,0 +1,31 @@
+# type: ignore
+
+morseCode = { 'A':'.-', 'B':'-...',
+    'C':'-.-.', 'D':'-..', 'E':'.',
+    'F':'..-.', 'G':'--.', 'H':'....',
+    'I':'..', 'J':'.---', 'K':'-.-',
+    'L':'.-..', 'M':'--', 'N':'-.',
+    'O':'---', 'P':'.--.', 'Q':'--.-',
+    'R':'.-.', 'S':'...', 'T':'-',
+    'U':'..-', 'V':'...-', 'W':'.--',
+    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+    '1':'.----', '2':'..---', '3':'...--',
+    '4':'....-', '5':'.....', '6':'-....',
+    '7':'--...', '8':'---..', '9':'----.',
+    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+    '?':'..--..', '/':'-..-.', '-':'-....-',
+    '(':'-.--.', ')':'-.--.-'}
+
+while True:
+    print(f"Input text to translate to morse code, or press -q to quit")
+    txt = input().split(' ')
+    
+    translation = []
+    for i in range(len(txt)):
+        upperCase = txt[i].upper()
+        for letter in upperCase:
+            translation.append(morseCode[letter])
+            translation.append(' ')
+        translation.append('/')
+    
+    print(translation)
